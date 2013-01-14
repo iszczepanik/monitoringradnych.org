a5ab818087fd903d906594c71327839b36e47d48
iszczepanik <iszczepanik@neubloc.net>
2013-01-14 21:03:55 +0100

    Jan-14 Google Analytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -163,6 +163,16 @@
 </div><!-- page -->
 <script type="text/javascript">
 
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-37658552-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
     $(document).ready(function() {
         //$('#slider1').s3Slider({
          //   timeOut: 4000 
